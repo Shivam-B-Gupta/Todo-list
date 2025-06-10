6cb200204a282f1a7478cf39ed90c883ffab5496
Shivam <sshivamgupta833@gmail.com>
2025-06-10 20:22:03 +0530

    added cors

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+// Allow requests from frontend origin
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
+
 app.use(express.json())
 
 const {userRoutes} = require('./routes/user');
